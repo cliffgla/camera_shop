29c95c0bde63f1abe5258840d107c83c4fbad3ac
cliffgla <cliffordgagliardo@gmail.com>
2017-08-23 17:13:01 +0200

    added dialogue to SLR function

--- photo_id.py
+++ photo_id.py
@@ -23,8 +23,29 @@ def rangefinder():
         print("We recommend Leica.")
 
 def slr():
-    print("We recommend an SLR camera")
-    print("Do you currently own Nikon or Canon lenses? [Y/n]")
+    while True:
+        print("We recommend an SLR camera")
+        print("Do you currently own Nikon or Canon lenses? [Y/n]")
+        user_lens = input("> ")
+        print(user_lens)
+        if user_lens.lower() == "y":
+            print("Nikon or Canon?")
+            lens_quest = input("> ")
+            print(lens_quest)
+            if lens_quest.lower() == "nikon":
+                print("Good news! Almost every current Nikon SLR is compatible!")
+                break
+            if lens_quest.lower() == "canon":
+                print("If your lenses were made before 1987,",
+                "you will have to buy new lenses, in which case",
+                "we recommend either a Nikon or Canon SLR.")
+                break
+        elif user_lens.lower() == "n":
+            print("Flip a coin: Canon and Nikon are both great.")
+            break
+        else:
+            print("That is not a valid choice. Please enter Y or N.")
+
 
 def view_camera() :
     print("We recommend a 4x5 view camera. ")
